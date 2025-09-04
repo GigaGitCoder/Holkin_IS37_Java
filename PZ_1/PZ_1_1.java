d475e63d147e18b1058d88a9b7bd7f0d7454bb55
Hunter <120475102+GigaGitCoder@users.noreply.github.com>
2025-09-04 20:09:42 +0300

    modified:   .gitignore 	new file:   PZ_1/PZ_1_1.java 	new file:   PZ_1/PZ_1_2.java 	new file:   PZ_1/PZ_1_3.java

---
+++ PZ_1/PZ_1_1.java
@@ -0,0 +1,12 @@
+class myProgram1{
+    public static void main(String[] args) {
+        // System.out.println("# - Это комментарий ) Cтоп, это не тот язык");
+        
+        System.out.println("// - Это комментарий ) Это не сложно");
+
+        /* System.out.println("// - Это не комментарий ) Это не сложно");
+        System.out.println("// - Это комментарий ) Это сложно");
+        System.out.println("// - Это не комментарий ) Это не сложно");
+        System.out.println("можно даже попробовать многострочнный комментарий"); */
+    }
+}
---
+++ PZ_1/PZ_1_2.java
@@ -0,0 +1,12 @@
+class myProgram2{
+    public static void main(String[] args) {
+        // System.out.println("Я прекрасно понинаю разницу ");
+        System.out.print("Я прекрасно понимаю разницу ");
+        System.out.println("между Systen.out.println()");
+        // System.out.print("между System.out.printin()");
+        // System.out.println("и System.out.print(). ");
+        System.out.print("и System.out.print(). ");
+        // System.out.println("A ты?");
+        System.out.print("A ты?");
+    }
+}
